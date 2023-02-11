13ffbab36128c617d097cfcead9340a563639fcb
Rai Raksa <rairaksa@evermos.com>
2023-02-12 00:36:34 +0700

    feat: set color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      'dark': '#212121',
+      'dark-secondary': '#ececec',
+      'white': '#ffffff',
+      'slate': '#eaeef6',
+      'info': '#CEECFD',
+      'info-dark': '#1778b0',
+      'purple-primary': '#7784ee',
+      'purple-secondary': '#d2d7ff'
+    },
     extend: {},
     fontFamily: {
       'body': ['"Poppins"'],
