b97776bb5fa551ab5989a2cfeb8d9d2ac3031b6b
Rai Raksa <rairaksa@evermos.com>
2023-02-12 09:01:42 +0700

    update task activity

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
   theme: {
     colors: {
       'dark': '#212121',
-      'dark-secondary': '#ececec',
+      'dark-secondary' : '#717171',
+      'border-secondary': '#ececec',
       'white': '#ffffff',
       'slate': '#eaeef6',
       'info': '#CEECFD',
       'info-dark': '#1778b0',
       'purple-primary': '#7784ee',
-      'purple-secondary': '#d2d7ff'
+      'purple-secondary': '#d2d7ff',
+      'blue-primary': '#08a0f7'
     },
     extend: {},
     fontFamily: {
